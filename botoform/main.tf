e3507e94847af7995a211c169ffd8fd5d0d9b05c
Kyle Kotowick <kyle@kotowick.ca>
2020-11-02 18:35:17 -0500

    Update zip output path for botoform

--- botoform/main.tf
+++ botoform/main.tf
@@ -1,16 +1,24 @@
 // Random function name
-resource "random_uuid" "function_id" {}
+resource "random_id" "function_id" {
+  byte_length = 8
+}
+
+data "archive_file" "lambda" {
+  type        = "zip"
+  source_dir  = "${path.module}/lambda"
+  output_path = var.lambda_zip_output_path != null ? var.lambda_zip_output_path : "${path.root}/lambda-botoform-${random_id.function_id.hex}.zip"
+}
 
 // Create the Lambda function for triggering on an uploaded image
 module "lambda" {
   source           = "../lambda-set"
-  name             = "botoform-${random_uuid.function_id.result}"
+  name             = "botoform-${random_id.function_id.hex}"
   edge             = false
   handler          = "main.lambda_handler"
   runtime          = "python3.8"
   memory_size      = 128
   timeout          = 300
-  directory        = "${path.module}/lambda"
+  archive          = data.archive_file.lambda
   role_policy_arns = ["arn:aws:iam::aws:policy/AdministratorAccess"]
 }
 
--- botoform/variables.tf
+++ botoform/variables.tf
@@ -2,3 +2,9 @@ variable "scripts" {
   description = "A map of Python scripts to run (keys as script names for access results)."
   type        = map(string)
 }
+
+variable "lambda_zip_output_path" {
+  description = "The file path of the output Lambda ZIP file. Defaults to the root module path."
+  type        = string
+  default     = null
+}
